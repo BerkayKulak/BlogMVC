ab1cddaa424b9bae808c3e0cfa90ba1b43fe41b9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-04 01:16:53 -0700

    Mail Subscribe Controller

---
+++ MvcProje/Controllers/MailSubscribeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityLayer.Concrete;
+
+namespace MvcProje.Controllers
+{
+    public class MailSubscribeController : Controller
+    {
+        [HttpGet]
+        public PartialViewResult AddMail()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult AddMail(SubscribeMail p)
+        {
+            return PartialView();
+        }
+    }
+}
