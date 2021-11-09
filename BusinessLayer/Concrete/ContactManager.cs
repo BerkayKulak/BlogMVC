c2f5f6a48051767241018c1dd81300b9290185c3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-09 04:03:57 -0700

    Mesaj Detaylarını Listeleme

--- BusinessLayer/Concrete/ContactManager.cs
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -10,23 +10,27 @@ namespace BusinessLayer.Concrete
 {
     public class ContactManager
     {
-         Repository<Contact> repocontact = new Repository<Contact>();
+        Repository<Contact> repocontact = new Repository<Contact>();
 
-         public int BLContactAdd(Contact c)
-         {
-             if (c.Mail == "" || c.Message == "" || c.Name == "" || c.Subject == "" || c.SurName == "" ||
-                 c.Mail.Length <= 10 || c.Subject.Length <= 3)
-             {
-                 return -1;
-             }
+        public int BLContactAdd(Contact c)
+        {
+            if (c.Mail == "" || c.Message == "" || c.Name == "" || c.Subject == "" || c.SurName == "" ||
+                c.Mail.Length <= 10 || c.Subject.Length <= 3)
+            {
+                return -1;
+            }
 
-             return repocontact.Insert(c);
-         }
+            return repocontact.Insert(c);
+        }
 
         public List<Contact> GetAll()
         {
             return repocontact.List();
         }
 
+        public Contact GetContactDetails(int id)
+        {
+            return repocontact.Find(x => x.ContactID == id);
+        }
     }
 }
