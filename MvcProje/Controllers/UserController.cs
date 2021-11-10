9829a46d7ae56d56a4e92d5adf73de15b200b604
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-10 08:31:28 -0700

    Yazar İçin Partial Oluşturma

--- MvcProje/Controllers/UserController.cs
+++ MvcProje/Controllers/UserController.cs
@@ -11,12 +11,17 @@ namespace MvcProje.Controllers
     {
         // GET: User
         private UserProfileManager userProfile = new UserProfileManager();
-        public ActionResult Index(string p)
+        public ActionResult Index()
         {
-            var mail = (string) Session["Mail"];
+            return View();
+        }
+
+        public PartialViewResult Partial1(string p)
+        {
+            var mail = (string)Session["Mail"];
             p = mail;
             var profilevalues = userProfile.GetAuthorByMail(p);
-            return View(profilevalues);
+            return PartialView(profilevalues);
         }
 
     }
