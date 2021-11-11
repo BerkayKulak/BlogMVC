17c73dd30685160cba32360e1256fa8513a1aa12
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-11 05:19:04 -0700

    Admin Rol Sütunu

--- EntityLayer/Concrete/Admin.cs
+++ EntityLayer/Concrete/Admin.cs
@@ -15,8 +15,13 @@ namespace EntityLayer.Concrete
         [StringLength(20)]
         public string UserName { get; set; }
 
+        [StringLength(1)]
+        public string AdminRol { get; set; }
+
         [StringLength(20)]
         public string Password { get; set; }
+
+        
 
     }
 }
