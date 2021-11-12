19c8201f3c4b98ef7113817459efc46065ab8bd3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-12 10:33:04 -0700

    Abstract Interface

---
+++ BusinessLayer/Abstract/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAuthorService
+    {
+        List<Author> GetList();
+        void AuthorAdd(Author author);
+        Author GetByID(int id);
+        void AuthorDelete(Author author);
+        void AuthorUpdate(Author author);
+    }
+}
