e7146341b2c159b0819dfcdbd70ba805fbf027d7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-11-12 10:46:04 -0700

    Fluent Validation

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş geçemezsiniz");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori Açıklamasını Boş geçemezsiniz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakterli kategori girişi yapınız");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterli kategori girişi yapınız");
+        }
+    }
+}
